a5bd43a1844b29147f34c77484a9ec17b6b13e20
Fábio <fabiohennrique11@gmail.com>
2025-11-25 20:38:28 -0300

    Criação da classe Administrador que herda de usuarios, construtores, getters/setters e implementação dos metodos

---
+++ model/Administrador.java
@@ -0,0 +1,38 @@
+package model;
+
+import Main;
+import java.util.Scanner;
+
+public class Administrador extends Usuario {
+
+    private String cargo;
+
+    public Administrador(String nome, String username, String senha, String cargo) {
+        super(nome, username, senha);
+        this.cargo = cargo;
+    }
+
+    public Administrador(int id, String nome, String username, String senha, boolean ativo, String cargo) {
+        super(id, nome, username, senha, ativo);
+        this.cargo = cargo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    @Override
+    public String getDetalhes() {
+        return String.format("[ADMIN] ID: %d, Nome: %s, Cargo: %s",
+                super.getId(), getNome(), getCargo());
+    }
+
+    @Override
+    public void exibirMenuPrincipal(Scanner input) {
+        Main.menuAdmin(this, input);
+    }
+}
