64e21bde3219c4c0f7afcf9f661fc1ecee6d1a5d
Juanpablouf <jupablosv123@gmail.com>
2025-12-02 16:11:04 -0300

    feat: adicionar cadastro, autenticação e listagem de usuários

--- service/UsuarioService.java
+++ service/UsuarioService.java
@@ -14,4 +14,38 @@ public class UsuarioService {
     private VeiculoService veiculoService = new VeiculoService();
     private ManutencaoService manutencaoService = new ManutencaoService();
     private RegistroUsoService registroUsoService = new RegistroUsoService();
+
+    public void cadastrarAdministrador(String nome, String username, String senha, String cargo) {
+        Usuario usuarioExistente = usuarioRepository.buscarPorUsername(username);
+        if (usuarioExistente != null) {
+            System.err.println("ERRO no Cadastro: usuario " + username + " ja existe");
+            return;
+        }
+
+        Administrador novoAdmin = new Administrador(nome, username, senha, cargo);
+
+        usuarioRepository.salvarAdmin(novoAdmin);
+        System.out.println("Administrador " + nome + " cadastrado com sucesso!");
+    }
+
+    public Usuario autenticar(String username, String senha) {
+        Usuario usuarioEncontrado = usuarioRepository.buscarPorUsername(username);
+
+        if (usuarioEncontrado == null) {
+            System.out.println("Falha na autenticação: Usuário '" + username + "' não existe.");
+            return null;
+        }
+
+        if (usuarioEncontrado.getSenha().equals(senha)) {
+            System.out.println("Autenticacao bem-sucedida:" + username);
+            return usuarioEncontrado;
+        } else {
+            System.out.println("Falha na autenticacao: senha incorreta.");
+            return null;
+        }
+    }
+
+    public List<Usuario> listarTodosUsuarios() {
+        return usuarioRepository.listarTodosUsuarios();
+    }
 }
