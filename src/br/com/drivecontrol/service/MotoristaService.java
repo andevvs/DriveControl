91a1fef383b977d302e9a8971353b8a2dc5199bf
Juanpablouf <jupablosv123@gmail.com>
2025-12-08 11:33:54 -0300

    feat: adicionar método cadastrarMotorista

--- src/br/com/drivecontrol/service/MotoristaService.java
+++ src/br/com/drivecontrol/service/MotoristaService.java
@@ -21,5 +21,31 @@ public class MotoristaService {
         this.registroUsoRepository = new RegistroUsoRepository(this.veiculoRepository, this.motoristaRepository);
         this.veiculoService = new VeiculoService();
     }
-}
 
+    public boolean cadastrarMotorista(String nome, String userName, String senha, String setor, String cnh) {
+        try {
+            validarDadosMotorista(nome, userName, senha, setor, cnh);
+
+            if (motoristaRepository.buscarPorCnh(cnh) != null) {
+                System.err.println(" ERRO: CNH " + cnh + " já está cadastrada");
+                return false;
+            }
+
+            Motorista novoMotorista = new Motorista(nome, userName, senha, setor, cnh);
+            motoristaRepository.salvar(novoMotorista);
+
+            System.out.println(" Motorista cadastrado com sucesso!");
+            System.out.println("   Nome: " + nome);
+            System.out.println("   CNH: " + cnh);
+            System.out.println("   Setor: " + setor);
+            System.out.println("   Username: " + userName);
+
+            return true;
+
+        } catch (Exception e) {
+            System.err.println(" Erro ao cadastrar motorista: " + e.getMessage());
+        }
+
+        return false;
+    }
+}
